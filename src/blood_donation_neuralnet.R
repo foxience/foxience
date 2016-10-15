fff0fd728d343ca0f763aa898e5e04e97741f8c0
Foxience <foxience@foxience.delivn>
2016-10-16 00:15:41 +0800

    update blood donation neural net

--- src/blood_donation_neuralnet.R
+++ src/blood_donation_neuralnet.R
@@ -1,62 +1,67 @@
 library(neuralnet)
 library(tidyr)
 library(dplyr)
+library(gmodels)
 library(ggplot2)
 
-setwd('/home/rook/Projects/Code/r-source/assignment')
+setwd('assignment')
 
 bd <- read.csv('blood_donation.csv')
 
+head(bd)
+
 ggplot(bd, aes(Number.of.Donations, Total.Volume.Donated..c.c..)) + geom_jitter()
-
 ggplot(bd, aes(Number.of.Donations, Months.since.First.Donation)) + geom_jitter()
+ggplot(bd, aes(Number.of.Donations, Months.since.Last.Donation)) + geom_jitter()
+ggplot(bd, aes(Months.since.First.Donation, Months.since.Last.Donation)) + geom_jitter()
 
-bd_prepared <- bd %>%
+bd.prepared <- bd %>%
   rename(MonthsLD = Months.since.Last.Donation,
-          NumberDonations = Number.of.Donations,
-          TotalVolume = Total.Volume.Donated..c.c..,
-          MonthsFD = Months.since.First.Donation,
-          March.2007 = Made.Donation.in.March.2007) %>%
-  # mutate(March.2007 = as.factor(March.2007)) %>%
+        NumberDonations = Number.of.Donations,
+        TotalVolume = Total.Volume.Donated..c.c..,
+        MonthsFD = Months.since.First.Donation,
+        March.2007 = Made.Donation.in.March.2007) %>%
+  mutate(March.2007 = as.factor(March.2007)) %>%
   mutate(DonateDuration = MonthsFD - MonthsLD + 1) %>%
   mutate(DonationPerMonth = NumberDonations / DonateDuration) %>%
   mutate(MonthPerDonation = DonateDuration / NumberDonations) %>%
   select(NumberDonations,
         MonthsFD,
         MonthsLD,
-        TotalVolume,
+        # TotalVolume,
+        # DonateDuration,
         DonationPerMonth,
         MonthPerDonation,
         March.2007)
 
-ggplot(bd_prepared, aes(March.2007, MonthsFD)) +
+ggplot(bd.prepared, aes(March.2007, MonthsFD)) +
   geom_boxplot()
 
-ggplot(bd_prepared, aes(March.2007, MonthsLD)) +
+ggplot(bd.prepared, aes(March.2007, MonthsLD)) +
   geom_boxplot()
 
-ggplot(bd_prepared, aes(March.2007, NumberDonations)) +
+ggplot(bd.prepared, aes(March.2007, NumberDonations)) +
   geom_boxplot()
 
-ggplot(bd_prepared, aes(MonthsFD, MonthsLD, col = March.2007)) +
+ggplot(bd.prepared, aes(MonthsFD, MonthsLD, col = March.2007)) +
     geom_point()
 
-ggplot(bd_prepared, aes(TotalVolume, NumberDonations, col = March.2007)) +
+ggplot(bd.prepared, aes(TotalVolume, NumberDonations, col = March.2007)) +
   geom_point()
 
-n <- nrow(bd_prepared)  
+n <- nrow(bd.prepared)
 s <- sample(1:n, 2*n/3)
-bd.training <- bd_prepared[s,]
-bd.testing <- bd_prepared[-s,]
+bd.training <- bd.prepared[s,]
+bd.validating <- bd.prepared[-s,]
 
 bd.nn <- neuralnet(March.2007 ~ MonthsFD + MonthsLD + NumberDonations + DonationPerMonth + MonthPerDonation,
-                   bd.training, hidden = c(5), threshold = 0.01, stepmax = 20000)
+                   bd.training, hidden = c(5), threshold = 0.01, stepmax = 10000)
 
-bd.nn.results <- neuralnet::compute(bd.nn, select(bd.testing,
-                                                  MonthsFD,
-                                                  MonthsLD,
-                                                  NumberDonations,
-                                                  DonationPerMonth,
-                                                  MonthPerDonation))$net.result
-bd.nn.results <- round(bd.nn.results)
-cbind(bd.testing, bd.nn.results)
+bd.nn.validating <- neuralnet::compute(bd.nn, select(bd.validating,
+                                                    MonthsFD,
+                                                    MonthsLD,
+                                                    NumberDonations,
+                                                    DonationPerMonth,
+                                                    MonthPerDonation))
+
+CrossTable(bd.validating$March.2007, round(bd.nn.validating$net.result))
